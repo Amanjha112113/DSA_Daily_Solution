40f62e902ad7b3dfeb9830f67336a09ce9de39cb
Aman jha <167541974+Amanjha112113@users.noreply.github.com>
2026-05-25 12:48:47 +0530

    Time: 0 ms (100%), Space: 52.4 MB (25.19%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+
+        int pos = 0;
+        for(int i = 0; i < n; i++) {
+
+            if(i > pos) {
+                return false;
+            }
+            pos = max(pos, i + nums[i]);
+        }
+        return true;
+    }
+};
